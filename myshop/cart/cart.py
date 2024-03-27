e7193ce012cb2046ce7eddcfc79216588f423b66
DanaMukh <arinenumb@gmail.com>
2024-03-27 14:14:27 +0700

    fishished cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -15,7 +15,6 @@ class Cart:
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False ):
-          
           """
         Добавить товар в корзину либо обновить его количество.
         """
@@ -65,6 +64,17 @@ class Cart:
         Подсчитать все товарные позиции в корзине.
             """
         return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
+    
+    def clear(self):
+        # удалить корзину из сеанса
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+
     
 
 
